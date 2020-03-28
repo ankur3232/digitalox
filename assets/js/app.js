8f6709f4a8de31e7544fbd233c90eb787c1457f9
Ankur (HP) <ankur.agr32@gmail.com>
2020-03-27 20:30:05 -0700

    "Nav and Hero responsive"

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,22 @@
 let burger = document.querySelector('.toggle');
 let list = document.querySelector('.menu');
 let close = document.querySelector('.close-menu');
+let upperLine = document.querySelector('.ham-line1');
+let midLine = document.querySelector('.ham-line2');
+let lowerLine = document.querySelector('.ham-line3');
 
 burger.addEventListener('click', () =>{        
         list.classList.toggle('slide');
         close.classList.toggle('is-open');
+        upperLine.classList.toggle('upanim');
+        midLine.classList.toggle('midanim');
+        lowerLine.classList.toggle('downanim');
     });
 close.addEventListener('click', () =>{        
         list.classList.toggle('slide');
         close.classList.toggle('is-open');
+        upperLine.classList.toggle('upanim');
+        midLine.classList.toggle('midanim');
+        lowerLine.classList.toggle('downanim');
     });
+
